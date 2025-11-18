75c51d82700f854ba71a20fa7a5017dca0012c3f
Magesh Kuppan <tkmagesh77@gmail.com>
2025-11-18 20:00:43 +0530

    more on channels

---
+++ 01-concurrency/05-communication/13-demo-b.go
@@ -0,0 +1,21 @@
+// Share memory by communicating (using channels)
+
+package main
+
+import (
+	"fmt"
+)
+
+// consumer
+func main() {
+	ch := make(chan int)
+	go add(100, 200, ch)
+	result := <-ch
+	fmt.Println("result :", result)
+}
+
+// producer
+func add(x, y int, ch chan int) {
+	result := x + y
+	ch <- result
+}
---
+++ 01-concurrency/05-communication/13-demo-c.go
@@ -0,0 +1,24 @@
+// Share memory by communicating (using channels)
+
+package main
+
+import (
+	"fmt"
+)
+
+// consumer
+func main() {
+	ch := add(100, 200)
+	result := <-ch
+	fmt.Println("result :", result)
+}
+
+// producer
+func add(x, y int) chan int {
+	ch := make(chan int)
+	go func() {
+		result := x + y
+		ch <- result
+	}()
+	return ch
+}
