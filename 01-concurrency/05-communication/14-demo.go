3c69a51c1289635437c0f3c47038b28da531cffa
Magesh Kuppan <tkmagesh77@gmail.com>
2025-11-18 19:15:00 +0530

    updates

---
+++ 01-concurrency/05-communication/14-demo.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	/*
+		ch := make(chan int)
+		data := <-ch
+		ch <- 100
+		fmt.Println(data)
+	*/
+
+	ch := make(chan int)
+	ch <- 100
+	data := <-ch
+	fmt.Println(data)
+}
